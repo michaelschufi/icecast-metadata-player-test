9c58660b9e5c635b251221079b705b3b141d4ad9
Michael Schaufelberger <michael.schufi@gmail.com>
2021-06-23 12:20:33 +0200

    updates icecast-metadata-player to 1.3.2 and next to 11

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE_BUNDLE === "true",
-});
+// eslint-disable import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
 
-module.exports = withBundleAnalyzer({
+const withTM = require("next-transpile-modules")([
+  "icecast-metadata-player",
+  "mse-audio-wrapper",
+  "codec-parser",
+]);
+
+module.exports = withTM({
   publicRuntimeConfig: {},
-  future: {
-    webpack5: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 });
